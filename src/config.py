7258c6770b6bae1a3e84d4f2b51ff96132fe8001
inspiron5559 <bryogenin_haul@aleeas.com>
2021-03-07 20:29:24 +0800

    update

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 config = {
     "binance_keys": {
-                    "binanceaccount1":{"API_KEY": "", "SECRET_KEY": ""},
-                    "binanceaccount2": {"API_KEY": "", "SECRET_KEY": ""}
+                    "binanceaccount1":{"API_KEY": "Jy8bSfXwX4VrPNb4KK84J99MU8qjYd7damAnAGFJrzoVA1yjy73k49rCmSvx2U75", "SECRET_KEY": "2N3RgXqoUgVy03EDa8Izu8B1rhHq8IA62smvSoINFxcSXCK3OLg1X3VrkT5OPtS7"},
+                    "binanceaccount2": {"API_KEY": "9rRzQXgN9IF6LRdqXtR8yQw7EuT10GSFf9gtZ21QVRHqhBo3PVF61x6885Z2fnsV", "SECRET_KEY": "GPqEGEbPZ9J57npGrqtTuhcLdi3VzB2OuKDWpKr4JrQuFoStNL2OPg0yJ8Ut9Pwz"}
                     },
     "bitmex_keys": {
                     "bitmexaccount1":{"API_KEY": "", "SECRET_KEY": ""},
